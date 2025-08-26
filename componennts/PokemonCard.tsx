34ab3e45fde30c1439c3c1c1c3f781402c99526f
TomLayan1 <afolayantomisin1919@gmail.com>
2025-08-26 16:41:27 +0100

    Installed the react native safe area context library to help avoid the notch area of both android and ios devices

---
+++ componennts/PokemonCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PokemonCard:React.FC = () => {
+  return (
+    <View>
+      <Text>Pokemon Card</Text>
+    </View>
+  )
+}
+
+export default PokemonCard
