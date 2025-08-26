a42633930a312178bcfb55484ebf4a602e6b5807
TomLayan1 <afolayantomisin1919@gmail.com>
2025-08-26 17:03:25 +0100

    Styled the card View

--- componennts/PokemonCard.tsx
+++ componennts/PokemonCard.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const PokemonCard:React.FC = () => {
   return (
-    <View>
+    <View style={styles.cardContainer}>
       <Text>Pokemon Card</Text>
     </View>
   )
 }
 
 export default PokemonCard
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: "#ffffff",
+    padding: 7,
+    borderRadius: 15,
+    borderWidth: 2,
+    margin: 9,
+    ...Platform.select({
+      ios:{
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4
+      },
+      android: {
+        elevation: 10
+      }
+    })
+  }
+})
